19134bd67c079764f483c53a3ccc3f19207cd75b
Kharzette <6589537+Kharzette@users.noreply.github.com>
2019-06-04 17:56:13 -0500

    GameWindow and Instance for DrunkSpock.  Dotnet standard build type for InputLib

---
+++ DrunkSpock/GameWindow.cs
@@ -0,0 +1,223 @@
+using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using GLFW;
+using VulkanCore;
+using VulkanCore.Khr;
+
+using InputLib;
+using UtilityLib;
+
+using Monitor	=GLFW.Monitor;
+
+
+namespace DrunkSpock
+{
+	public class GameWindow
+	{
+		Window		mWnd;
+		SurfaceKhr	mSurface;
+
+		Input	mAttachedInput;
+
+		bool	bDestroyed;
+
+		//timings
+		long	mLastFrame;
+		long	mRenderAccum, mGameAccum;
+		long	mUpdateTicMS	=33;
+		long	mRenderTicMS	=33; 
+
+		public event EventHandler	eGameTic;
+		public event EventHandler	eRenderTic;
+		public event EventHandler	eWindowClosing;
+
+
+		public GameWindow(DrunkSpock ds, bool bResizable, string title,
+			int width, int height, int monitorIndex, bool bCenter)
+		{
+			//set GLFW_NO_API
+			Glfw.WindowHint(Hint.ClientApi, 0);
+			Glfw.WindowHint(Hint.Resizable, bResizable);
+
+			if(monitorIndex < 0 || monitorIndex >= Glfw.Monitors.Length)
+			{
+				monitorIndex	=0;
+			}
+
+			//my linux implementation has no monitor work area function
+			List<int>	xCoords	=new List<int>();
+			List<int>	yCoords	=new List<int>();
+			for(int i=0;i < Glfw.Monitors.Length;i++)
+			{
+				int	mx, my;
+				Glfw.GetMonitorPosition(Glfw.Monitors[i], out mx, out my);
+
+				xCoords.Add(mx);
+				yCoords.Add(my);
+			}
+
+			mWnd	=Glfw.CreateWindow(width, height, title,
+						Monitor.None, Window.None);
+
+			int	x, y;
+			Glfw.GetWindowPosition(mWnd, out x, out y);
+
+			//figure out which monitor the window is on
+			int	bestMonitorIndex	=-1;
+			int	bestDist			=Int32.MaxValue;
+			for(int i=0;i < Glfw.Monitors.Length;i++)
+			{
+				if(x > xCoords[i])
+				{
+					int	dist	=x - xCoords[i];
+					if(dist < bestDist)
+					{
+						bestMonitorIndex	=i;
+					}
+				}
+			}
+
+			//get current vidmode of monitors
+			List<VideoMode>	modes	=new List<VideoMode>();
+			for(int i=0;i < Glfw.Monitors.Length;i++)
+			{
+				modes.Add(Glfw.GetVideoMode(Glfw.Monitors[i]));
+			}
+
+			if(bCenter)
+			{
+				//center of desired monitor
+				x	=modes[monitorIndex].Width / 2;
+				y	=modes[monitorIndex].Height / 2;
+
+				//adjust back by half window size
+				x	-=width / 2;
+				y	-=height / 2;
+			}
+			else
+			{
+				//adjust to local coordiates
+				x	-=xCoords[bestMonitorIndex];
+				y	-=yCoords[bestMonitorIndex];
+			}
+
+			//add desired monitor xy
+			x	+=xCoords[monitorIndex];
+			y	+=yCoords[monitorIndex];
+
+			Glfw.SetWindowPosition(mWnd, x, y);
+
+			//create window surface
+			IntPtr	surfaceHandle;
+
+			Result	result	=(Result)Vulkan.CreateWindowSurface(
+				ds.GetInstance().Handle, mWnd, IntPtr.Zero, out surfaceHandle);
+			if(result != Result.Success)
+			{
+				ds.ErrorSpew("Window surface creation failed: " + result.ToString());
+				Glfw.DestroyWindow(mWnd);
+				Glfw.Terminate();
+				return;
+			}
+
+			AllocationCallbacks?	superAnnoyingParameter	=null;
+
+			mSurface	=new SurfaceKhr(ds.GetInstance(),
+				ref superAnnoyingParameter, surfaceHandle.ToInt64());
+
+			mLastFrame	=Stopwatch.GetTimestamp();
+		}
+
+
+		public void SetTicRate(long updateTic, long renderTic)
+		{
+			mUpdateTicMS	=updateTic;
+			mRenderTicMS	=renderTic;
+		}
+
+
+		public SurfaceKhr GetSurface()
+		{
+			return	mSurface;
+		}
+
+
+		//return true to quit
+		public bool GameLoop()
+		{
+			if(bDestroyed)
+			{
+				return	true;
+			}
+			Glfw.PollEvents();
+
+			if(Glfw.WindowShouldClose(mWnd))
+			{
+				Misc.SafeInvoke(eWindowClosing, null);
+				return	true;
+			}
+
+			long	now		=Stopwatch.GetTimestamp();
+			long	delta	=now - mLastFrame;
+
+			//deltatime in ms
+			delta	/=(Stopwatch.Frequency / 1000);
+
+			mRenderAccum	+=Math.Max(delta, 1);
+			mGameAccum		+=Math.Max(delta, 1);
+
+			if(mGameAccum > mUpdateTicMS)
+			{
+				Nullable<long>	deltaTime	=mGameAccum;
+				Misc.SafeInvoke(eGameTic, deltaTime);
+				mGameAccum	=0;
+			}
+			if(mRenderAccum > mRenderTicMS)
+			{
+				Nullable<long>	deltaTime	=mRenderAccum;
+				Misc.SafeInvoke(eRenderTic, null);
+				mRenderAccum	=0;
+			}
+			mLastFrame	=now;
+
+			//sleep a bit if delta times are low
+			if(delta < 5)
+			{
+				Thread.Sleep(5);
+			}
+
+			return	false;
+		}
+
+
+		public void AttachInput(InputLib.Input inp)
+		{
+			mAttachedInput	=inp;
+
+			Glfw.SetKeyCallback(mWnd, KeyCB);
+		}
+
+
+		public void Destroy()
+		{
+			mSurface.Dispose();
+			Glfw.DestroyWindow(mWnd);
+			Glfw.Terminate();
+			bDestroyed	=true;
+		}
+
+
+		void KeyCB(IntPtr window, Keys key, int scanCode,
+			InputState state, ModiferKeys mods)
+		{
+/*			Console.WriteLine("wnd: " + window + ", key: " + key +
+				", scanCode: " + scanCode + ", InputState: "
+				+ state.ToString() + ", mods: " + mods.ToString());
+*/
+			mAttachedInput.ProcessKeyEvent(key, state, mods);
+		}
+	}
+}
---
+++ DrunkSpock/Instance.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+
+using GLFW;
+using VulkanCore;
+using VulkanCore.Ext;
+using VulkanCore.Khr;
+
+using VER	=VulkanCore.Version;
+using INST	=VulkanCore.Instance;
+
+
+namespace DrunkSpock
+{
+	internal class Instance
+	{
+		INST	mInstance;
+
+
+		internal Instance(string appName, string engName,
+			VER appVer, VER engVer, VER apiVer, bool bDebug)
+		{
+			string	vlName	="";
+			if(bDebug)
+			{
+				if(!bCheckValidation(out vlName))
+				{
+					return;
+				}
+			}
+
+			ApplicationInfo	ai		=new ApplicationInfo();
+			ai.ApplicationName		=appName;
+			ai.ApplicationVersion	=appVer;
+			ai.EngineName			=engName;
+			ai.EngineVersion		=engVer;
+			ai.ApiVersion			=apiVer;
+
+			InstanceCreateInfo	ici	=new InstanceCreateInfo();
+			ici.Next				=IntPtr.Zero;
+			ici.ApplicationInfo		=ai;
+
+			List<string>	extensions	=new List<string>();
+
+			extensions.AddRange(Vulkan.GetRequiredInstanceExtensions());
+			if(bDebug)
+			{
+				extensions.Add(Constant.InstanceExtension.ExtDebugReport);
+			}
+
+			//get extension stuff from glfw
+			ici.EnabledExtensionNames	=extensions.ToArray();
+
+			if(bDebug)
+			{
+				ici.EnabledLayerNames	=new string[1] { vlName };
+			}
+			
+			mInstance	=new INST(ici, null);
+		}
+
+
+		internal INST GetInstance()
+		{
+			return	mInstance;
+		}
+
+
+		internal bool IsValid()
+		{
+			return	(mInstance != null);
+		}
+
+
+		internal void Destroy()
+		{
+			mInstance.Dispose();
+		}
+
+
+		internal DebugReportCallbackExt AttachDebugCallback(DebugReportCallbackCreateInfoExt drcbci)
+		{
+			return	mInstance.CreateDebugReportCallbackExt(drcbci);
+		}
+
+
+		bool bCheckValidation(out string vlName)
+		{
+			LayerProperties	[]lps	=INST.EnumerateLayerProperties();
+
+			bool	bFound	=false;
+			vlName			="";
+			foreach(LayerProperties lp in lps)
+			{
+				if(lp.Description.Contains("Standard Validation Layer"))
+				{
+					bFound	=true;
+					vlName	=lp.LayerName;
+				}
+			}
+
+			if(!bFound)
+			{
+				Console.WriteLine("Missing standard validation layer...");
+				return	false;
+			}
+			return	true;
+		}
+	}
+}
