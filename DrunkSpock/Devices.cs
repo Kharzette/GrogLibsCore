f8b79119d31b00ec89ac9f6559e634b3aa697478
Kharzette <6589537+Kharzette@users.noreply.github.com>
2019-06-05 19:43:39 -0500

    Far more sane Devices.cs Puts more of the burden on the app instead of trying to solve all the queues for them

--- DrunkSpock/Devices.cs
+++ DrunkSpock/Devices.cs
@@ -17,28 +17,18 @@ namespace DrunkSpock
 {
 	public class Devices
 	{
-		public enum QueueTypes
-		{
-			Compute,
-			Graphics,
-			SparseBinding,
-			Transfer,
-			Present,				//prefer queue just for present
-			GraphicsPlusPresent		//prefer g & p combined
-		};
-
 		//physical devices
 		List<PhysicalDevice>	mPhysicals	=new List<PhysicalDevice>();
 
 		//created logical devices
-		List<Device>	mLogicals	=new List<Device>();
+		Dictionary<string, Device>	mLogicals	=new Dictionary<string, Device>();
 
 		//physical device data
 		List<PhysicalDeviceProperties>	mDeviceProps			=new List<PhysicalDeviceProperties>();
 		List<PhysicalDeviceFeatures>	mDeviceFeatures			=new List<PhysicalDeviceFeatures>();
 		List<QueueFamilyProperties[]>	mDeviceQueueFamProps	=new List<QueueFamilyProperties[]>();
 
-		//device key, value is indexes of queue families
+		//phys device key, value is indexes of queue families
 		Dictionary<int, List<int>>	mGraphicsIndexes	=new Dictionary<int, List<int>>();
 		Dictionary<int, List<int>>	mComputeIndexes		=new Dictionary<int, List<int>>();
 		Dictionary<int, List<int>>	mTransferIndexes	=new Dictionary<int, List<int>>();
@@ -50,16 +40,8 @@ namespace DrunkSpock
 		//queue family limits keyed on device index
 		Dictionary<int, List<int>>	mQueueLimits	=new Dictionary<int, List<int>>();
 
-		//queues associated with device
-		Dictionary<Device, List<Queue>>	mQueues	=new Dictionary<Device, List<Queue>>();
-
 		//queue names
 		Dictionary<string, Queue>	mQueueNames		=new Dictionary<string, Queue>();
-
-		//wanted queues to be made
-		List<QueueTypes>	mWantQ			=new List<QueueTypes>();
-		List<string>		mWantNames		=new List<string>();
-		List<float>			mWantPriorities	=new List<float>();
 
 		internal event EventHandler	eErrorSpam;
 
@@ -70,120 +52,136 @@ namespace DrunkSpock
 		}
 
 
-		int TotalWanted(QueueTypes kind)
+		public int GetNumPhysicalDevices()
 		{
-			int	total	=0;
-			foreach(QueueTypes qt in mWantQ)
-			{
-				if(qt == kind)
-				{
-					total++;
-				}
-			}
-			return	total;
+			return	mPhysicals.Count;
 		}
 
 
-		int TotalPossible(QueueTypes kind)
+		public PhysicalDeviceProperties GetPhysicalDeviceProps(int index)
 		{
-			int	total	=0;
-			if(kind == QueueTypes.Compute)
-			{
-				foreach(KeyValuePair<int, List<int>> qfi in mComputeIndexes)
-				{
-					foreach(int idx in qfi.Value)
-					{
-						total	+=mQueueLimits[qfi.Key][idx];
-					}
-				}
-			}
-			else if(kind == QueueTypes.Graphics)
-			{
-				foreach(KeyValuePair<int, List<int>> qfi in mGraphicsIndexes)
-				{
-					foreach(int idx in qfi.Value)
-					{
-						total	+=mQueueLimits[qfi.Key][idx];
-					}
-				}
-			}
-			else if(kind == QueueTypes.GraphicsPlusPresent)			
-			{
-				foreach(KeyValuePair<int, List<int>> qfi in mGraphicsIndexes)
-				{
-					foreach(int idx in qfi.Value)
-					{
-						if(mbPresentSupport[qfi.Key][idx])
-						{
-							total	+=mQueueLimits[qfi.Key][idx];
-						}
-					}
-				}
-			}
-			else if(kind == QueueTypes.Present)
-			{
-				foreach(KeyValuePair<int, List<bool>> qps in mbPresentSupport)
-				{
-					foreach(bool b in qps.Value)
-					{
-						if(b)
-						{
-							total++;
-						}
-					}
-				}
-			}
-			else if(kind == QueueTypes.SparseBinding)
-			{
-				foreach(KeyValuePair<int, List<int>> qfi in mSparseIndexes)
-				{
-					foreach(int idx in qfi.Value)
-					{
-						total	+=mQueueLimits[qfi.Key][idx];
-					}
-				}
-			}
-			else if(kind == QueueTypes.Transfer)
-			{
-				foreach(KeyValuePair<int, List<int>> qfi in mTransferIndexes)
-				{
-					foreach(int idx in qfi.Value)
-					{
-						total	+=mQueueLimits[qfi.Key][idx];
-					}
-				}
-			}
-			return	total;
+			return	mDeviceProps[index];
 		}
 
 
-		public bool WantQueue(QueueTypes kind, string name, float priority)
+		public PhysicalDeviceFeatures GetPhysicalDeviceFeatures(int index)
 		{
-			//make sure name is unique
-			if(mWantNames.Contains(name))
+			return	mDeviceFeatures[index];
+		}
+
+
+		public bool bSupportsPresent(int physIndex, int famIndex)
+		{
+			if(physIndex < 0 || physIndex >= mPhysicals.Count)
 			{
-				Misc.SafeInvoke(eErrorSpam, "Name: " + name + " already in use!");
+				Misc.SafeInvoke(eErrorSpam, "Physical index out of range in bSupportsPresent()");
+				return	false;
+			}
+			if(famIndex < 0 || famIndex >= mDeviceQueueFamProps[physIndex].Length)
+			{
+				Misc.SafeInvoke(eErrorSpam, "Bad queue family index: " + famIndex + " in bSupportsPresent()");
+				return	false;
+			}
+			return	mbPresentSupport[physIndex][famIndex];
+		}
+
+
+		public List<int> GetFamilyIndexesFor(Queues qTypes, int physIndex)
+		{
+			switch(qTypes)
+			{
+				case	Queues.Compute:
+					return	mComputeIndexes[physIndex];
+				case	Queues.Graphics:
+					return	mGraphicsIndexes[physIndex];
+				case	Queues.SparseBinding:
+					return	mSparseIndexes[physIndex];
+				case	Queues.Transfer:
+					return	mTransferIndexes[physIndex];
+			}
+			return	null;
+		}
+
+
+		public bool CreateLogicalDevice(string name, int physIndex,
+			List<DeviceQueueCreateInfo> queues, List<string> extensions,
+			Nullable<PhysicalDeviceFeatures> features)
+		{
+			if(physIndex < 0 || physIndex >= mPhysicals.Count)
+			{
+				Misc.SafeInvoke(eErrorSpam, "Physical index out of range in CreateLogicalDevice()");
 				return	false;
 			}
 
-			//make sure there's room for it
-			if(TotalWanted(kind) < TotalPossible(kind))
+			if(mLogicals.ContainsKey(name))
 			{
-				mWantQ.Add(kind);
-				mWantNames.Add(name);
-				mWantPriorities.Add(priority);
-				return	true;
+				Misc.SafeInvoke(eErrorSpam, "Logical device name: " + name + " already in use in CreateLogicalDevice()");
+				return	false;
 			}
-			return	false;
+
+			DeviceCreateInfo	dci	=new DeviceCreateInfo(queues.ToArray(), null);
+
+			if(extensions != null && extensions.Count > 0)
+			{
+				dci.EnabledExtensionNames	=extensions.ToArray();
+			}
+
+			if(features != null)
+			{
+				dci.EnabledFeatures	=features.Value;
+			}
+
+			Device	d	=mPhysicals[physIndex].CreateDevice(dci);
+
+			mLogicals.Add(name, d);
+
+			return	true;
 		}
 
 
-		public void CreateSwapChain(int physIndex, SurfaceKhr surf)
+		public bool SetQueueName(string logName, string qName,
+			int	famIndex, int qIndex)
 		{
-			PhysicalDevice	phys	=mPhysicals[physIndex];
-			Device			dv		=mLogicals[physIndex];
+			if(!mLogicals.ContainsKey(logName))
+			{
+				Misc.SafeInvoke(eErrorSpam, "Bad logical device name: " + logName + " in SetQueueName()");
+				return	false;
+			}
 
-			Debug.Assert(dv.Parent == phys);
+			if(mQueueNames.ContainsKey(qName))
+			{
+				Misc.SafeInvoke(eErrorSpam, "Queue name already in use: " + qName + " in SetQueueName()");
+				return	false;
+			}
+
+			Device	dv	=mLogicals[logName];
+
+			int	physIndex	=mPhysicals.IndexOf(dv.Parent);
+
+			if(famIndex < 0 || famIndex >= mDeviceQueueFamProps[physIndex].Length)
+			{
+				Misc.SafeInvoke(eErrorSpam, "Bad queue family index: " + famIndex + " in SetQueueName()");
+				return	false;
+			}
+
+			if(qIndex < 0 || qIndex >= mQueueLimits[physIndex][qIndex])
+			{
+				Misc.SafeInvoke(eErrorSpam, "Bad queue index: " + qIndex + " in SetQueueName()");
+				return	false;
+			}
+
+			Queue	q	=dv.GetQueue(famIndex, qIndex);
+
+			mQueueNames.Add(qName, q);
+
+			return	true;
+		}
+
+
+		public bool CreateSwapChain(string logName, SurfaceKhr surf)
+		{
+			Device			dv		=mLogicals[logName];
+			PhysicalDevice	phys	=dv.Parent;
 
 			SurfaceCapabilitiesKhr	surfCaps		=phys.GetSurfaceCapabilitiesKhr(surf);
 			SurfaceFormatKhr		[]surfFormats	=phys.GetSurfaceFormatsKhr(surf);
@@ -192,7 +190,7 @@ namespace DrunkSpock
 			if(surfFormats.Length <=0 || presModes.Length <= 0)
 			{
 				Misc.SafeInvoke(eErrorSpam, "Bad formats or pres modes...");
-				return;
+				return	false;
 			}
 
 			Extent2D	chainExtent	=new Extent2D(1280, 720);
@@ -203,6 +201,11 @@ namespace DrunkSpock
 				ImageUsages.ColorAttachment);
 
 			SwapchainKhr	swapChain	=dv.CreateSwapchainKhr(scci);
+			if(swapChain == null)
+			{
+				Misc.SafeInvoke(eErrorSpam, "Create swap chain failed...");
+				return	false;
+			}
 
 			VulkanCore.Image	[]chainImages		=swapChain.GetImages();
 			ImageView			[]chainImageViews	=new ImageView[chainImages.Length];
@@ -217,166 +220,15 @@ namespace DrunkSpock
 
 				chainImageViews[i]	=chainImages[i].CreateView(ivci);
 			}
-
-		}
-
-
-		public void CreateLogicals()
-		{
-			//total up the queues wanted
-			int	computeWanted		=mWantQ.Count(q => q == QueueTypes.Compute);
-			int	graphicsWanted		=mWantQ.Count(q => q == QueueTypes.Graphics);
-			int	graPlusPresWanted	=mWantQ.Count(q => q == QueueTypes.GraphicsPlusPresent);
-			int	presWanted			=mWantQ.Count(q => q == QueueTypes.Present);
-			int	sparseWanted		=mWantQ.Count(q => q == QueueTypes.SparseBinding);
-			int	transWanted			=mWantQ.Count(q => q == QueueTypes.Transfer);
-
-			int	totalWanted	=computeWanted + graphicsWanted + graPlusPresWanted +
-								presWanted + sparseWanted + transWanted;
-
-			//track family spots left
-			Dictionary<int, List<int>>	famQueueRemaining	=
-				new Dictionary<int, List<int>>(mQueueLimits);
-
-			List<DeviceQueueCreateInfo>	qci	=new List<DeviceQueueCreateInfo>();
-			List<int>					qd	=new List<int>();
-
-			for(int i=0;i < mWantQ.Count;i++)
-			{
-				//find a queue spot
-				int	dev			=-1;
-				int	famIndex	=-1;
-
-				Dictionary<int, List<int>>	indexer;
-				switch(mWantQ[i])
-				{
-					case	QueueTypes.Compute:
-						indexer	=mComputeIndexes;
-						break;
-					case	QueueTypes.Graphics:
-						indexer	=mGraphicsIndexes;
-						break;
-					case	QueueTypes.GraphicsPlusPresent:
-						indexer	=mGraphicsIndexes;
-						break;
-					case	QueueTypes.Present:
-						indexer	=mGraphicsIndexes;	//present queues always graphics?
-						break;
-					case	QueueTypes.SparseBinding:
-						indexer	=mSparseIndexes;
-						break;
-					case	QueueTypes.Transfer:
-						indexer	=mTransferIndexes;
-						break;
-					default:
-						indexer	=mTransferIndexes;	//?
-						break;
-				}
-				foreach(KeyValuePair<int, List<int>> ci in indexer)
-				{
-					foreach(int idx in ci.Value)
-					{
-						if(famQueueRemaining[ci.Key][idx] > 0)
-						{
-							dev			=ci.Key;
-							famIndex	=idx;
-							break;
-						}
-					}
-					if(dev != -1)
-					{
-						break;
-					}
-				}
-				DeviceQueueCreateInfo	dqci	=new DeviceQueueCreateInfo(
-					famIndex, 1, new float[] { mWantPriorities[i] });
-				qci.Add(dqci);
-				qd.Add(dev);
-			}
-
-			//split by device
-			for(int dev=0;dev < mPhysicals.Count;dev++)
-			{
-				List<DeviceQueueCreateInfo>	perDev	=new List<DeviceQueueCreateInfo>();
-
-				bool	bAnyPresent	=false;
-				for(int i=0;i < qci.Count;i++)
-				{
-					if(qd[dev] == dev)
-					{
-						perDev.Add(qci[i]);
-
-						if(mWantQ[i] == QueueTypes.GraphicsPlusPresent
-							|| mWantQ[i] == QueueTypes.Present)
-						{
-							bAnyPresent	=true;							
-						}
-					}
-				}
-
-				DeviceCreateInfo	dci	=new DeviceCreateInfo(perDev.ToArray(), null);
-
-				if(bAnyPresent)
-				{
-					dci.EnabledExtensionNames	=new string[1] { "VK_KHR_swapchain" };
-				}
-				Device	d	=mPhysicals[dev].CreateDevice(dci);
-
-				mLogicals.Add(d);
-				mQueues.Add(d, new List<Queue>());
-
-				//count up families in use
-				Dictionary<int, int>	famsInUse	=new Dictionary<int, int>();
-
-				for(int i=0;i < perDev.Count;i++)
-				{
-					int	qfi	=perDev[i].QueueFamilyIndex;
-					if(famsInUse.ContainsKey(qfi))
-					{
-						famsInUse[qfi]++;
-					}
-					else
-					{
-						famsInUse.Add(qfi, 1);
-					}
-				}
-
-				//track next index
-				Dictionary<int, int>	nextIndex	=new Dictionary<int, int>();
-				foreach(KeyValuePair<int, int> fiu in famsInUse)
-				{
-					nextIndex.Add(fiu.Key, 0);
-				}
-
-				//grab queues
-				for(int i=0;i < perDev.Count;i++)
-				{
-					int	ogIdx	=qci.IndexOf(perDev[i]);
-					int	qfIdx	=perDev[i].QueueFamilyIndex;
-
-					Queue	q	=d.GetQueue(qfIdx, nextIndex[qfIdx]);
-
-					nextIndex[qfIdx]++;
-
-					//add by name
-					mQueueNames.Add(mWantNames[ogIdx], q);
-
-					//associate with device
-					mQueues[d].Add(q);
-				}
-			}
+			return	true;
 		}
 
 
 		void ExaminePhysicalDevices(Instance inst, SurfaceKhr surf)
 		{
 			PhysicalDevice	[]devs	=inst.GetInstance().EnumeratePhysicalDevices();
-			if(devs.Length <= 0)
-			{
-//				ErrorSpew("No physical devices returned from Enumeration.");
-//				Destroy();
-				return;
-			}
+
+			Debug.Assert(devs.Length > 0);
 
 			mPhysicals.AddRange(devs);
 
---
+++ UtilityLib/Misc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UtilityLib
+{
+	public static partial class Misc
+	{
+		public static void SafeInvoke(this EventHandler eh, object sender)
+		{
+			if(eh != null)
+			{
+				eh(sender, EventArgs.Empty);
+			}
+		}
+
+
+		public static void SafeInvoke(this EventHandler eh, object sender, EventArgs ea)
+		{
+			if(eh != null)
+			{
+				eh(sender, ea);
+			}
+		}
+
+
+		public static void SafeInvoke<T>(this EventHandler<T> eh, object sender, T ea) where T : EventArgs
+		{
+			if(eh != null)
+			{
+				eh(sender, ea);
+			}
+		}
+	}
+}
